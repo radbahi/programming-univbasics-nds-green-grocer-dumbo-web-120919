d7e3ce9d95184be44308c62ea206a44af590efa0
Radouane Bahi <radbahi@gmail.com>
2019-12-03 17:06:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -75,7 +75,9 @@ def apply_clearance(cart)
   i = 0 
   while i < cart.length do 
     if cart[i][:clearance]
-      cart[i][:price] = (cart[i][:price] - (cart[i][:price] * 0.20)).round2
+      cart[i][:price] = (cart[i][:price] - (cart[i][:price] * 0.20)).round(2)
+end
+i += 1 
 end
 
 def checkout(cart, coupons)
