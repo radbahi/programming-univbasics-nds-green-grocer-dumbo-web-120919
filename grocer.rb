9ed6d6d81a018a72a59cd097628aa8f6f7bab3c0
Radouane Bahi <radbahi@gmail.com>
2019-12-03 15:32:35 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,6 +13,7 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
+  
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
